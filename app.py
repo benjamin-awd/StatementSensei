807e78db19e736b1b3d0445e6fe30eac42f164cd
Benjamin Dornel <benjamindornel@gmail.com>
2024-04-20 00:49:10 +0800

    feat: improve password handling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from tempfile import NamedTemporaryFile
 
 import streamlit as st
+from uuid import uuid4
 from monopoly.processors import detect_processor
 from pydantic import SecretStr
 
@@ -11,23 +12,29 @@ def parse_bank_statement(file_path: str, password: str = None):
     transformed_df = processor.transform(statement)
     st.dataframe(transformed_df, use_container_width=True)
 
-
 uploaded_file = st.file_uploader("Choose a .pdf file", type="pdf")
-if uploaded_file is not None:
-    with NamedTemporaryFile(dir=".", suffix=".pdf") as file:
-        file.write(uploaded_file.getbuffer())
 
-        try:
+if uploaded_file:
+    try:
+        with NamedTemporaryFile(dir=".", suffix=".pdf") as file:
+            file.write(uploaded_file.getbuffer())
             parse_bank_statement(file.name)
+    except ValueError:
+        password = st.text_input(
+            label="Password",
+            type="password",
+            placeholder="Enter password",
+        )
 
-        except ValueError:
-            password = st.text_input(
-                label="password",
-                type="password",
-                placeholder="Password",
-                key="widget",
-                label_visibility="hidden",
-            )
+        submit_button = st.button("Submit")
+
+        if submit_button:
             if not password:
-                st.stop()
-            parse_bank_statement(file.name, password)
+                st.warning("Please enter a password.")
+            else:
+                try:
+                    with NamedTemporaryFile(dir=".", suffix=".pdf") as file:
+                        file.write(uploaded_file.getbuffer())
+                        parse_bank_statement(file.name, password)
+                except ValueError:
+                    st.error("Wrong password. Please try again.")
