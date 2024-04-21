8d7cc52891c90ea96e70a59f0b79ea35413e3918
Benjamin Dornel <benjamindornel@gmail.com>
2024-04-21 18:43:05 +0800

    feat: add download button

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ uploaded_file = st.file_uploader("Upload a bank statement", type="pdf")
 if uploaded_file:
     file_bytes = uploaded_file.read()
     document = fitz.Document(stream=file_bytes)
-    
+    document._name = uploaded_file.name
+
     if document.is_encrypted:
         password = st.text_input(
             label="Password",
@@ -32,8 +33,7 @@ if uploaded_file:
             else:
                 document.authenticate(password)
                 try:
-                    file_bytes = document.tobytes()
-                    parse_bank_statement(file_bytes, password)
+                    parse_bank_statement(document, password)
 
                 except ValueError as err:
                     if err.args[0] == "document closed or encrypted":
@@ -45,4 +45,4 @@ if uploaded_file:
                 except UnsupportedBankError:
                     st.error("This bank is not currently supported")
     else:
-        parse_bank_statement(file_bytes)
+        parse_bank_statement(document)
--- helpers.py
+++ helpers.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import streamlit as st
+from fitz import Document
 from pydantic import SecretStr
 
 from monopoly.processors import detect_processor
 
 
-def parse_bank_statement(file_bytes: bytes, password: str = None) -> None:
-    processor = detect_processor(file_bytes=file_bytes, passwords=[SecretStr(password)])
+def parse_bank_statement(document: Document, password: str = None) -> None:
+    processor = detect_processor(file_bytes=document.tobytes(), passwords=[SecretStr(password)])
     statement = processor.extract()
     df = processor.transform(statement)
     df["transaction_date"] = pd.to_datetime(df["transaction_date"]).dt.date
@@ -14,3 +15,10 @@ def parse_bank_statement(file_bytes: bytes, password: str = None) -> None:
     st.dataframe(df, use_container_width=True, hide_index=True)
     total_balance = df["Amount"].sum()
     st.write(f"Total Balance: ${total_balance:.2f}")
+    csv = df.to_csv(index=False).encode("utf-8")
+    st.download_button(
+        label="Download CSV",
+        data=csv,
+        file_name=f"{document.name.split('.')[0]}.csv",
+        mime="text/csv"
+    )
