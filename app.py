dc712b03731bc4bafbdbcce675b6ff97c9706da2
Benjamin Dornel <benjamindornel@gmail.com>
2024-06-13 23:01:12 +0800

    feat: add support for multiple files

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import fitz
+import pandas as pd
 import streamlit as st
 from monopoly.pdf import WrongPasswordError
 
-from helpers import parse_bank_statement
+from helpers import format_df, parse_bank_statement
 
 st.set_page_config(page_title="Monopoly", layout="wide")
 
@@ -14,37 +15,66 @@ Effortlessly extract transactions from PDF bank statements.
 """
 )
 
-uploaded_file = st.file_uploader(
-    label="Upload a bank statement", type="pdf", label_visibility="hidden"
-)
 
-if uploaded_file:
-    file_bytes = uploaded_file.read()
-    document = fitz.Document(stream=file_bytes)
-    document.pdf_file_name = uploaded_file.name
+def app() -> None:
+    uploaded_files = st.file_uploader(
+        label="Upload a bank statement",
+        type="pdf",
+        label_visibility="hidden",
+        accept_multiple_files=True,
+    )
 
-    if document.is_encrypted:  # pylint: disable=no-member
-        password = st.text_input(
-            label="Password",
-            type="password",
-            placeholder="Enter password",
+    dataframes = []
+
+    for file in uploaded_files:
+        if file:
+            df = None
+            file_bytes = file.read()
+            document = fitz.Document(stream=file_bytes)
+
+            if document.is_encrypted:  # pylint: disable=no-member
+                password_container = st.empty()
+                password = password_container.text_input(
+                    label="Password",
+                    type="password",
+                    placeholder=f"Enter password for {file.name}",
+                    key=file.name,
+                )
+
+                if not password:
+                    st.warning("Please enter a password.")
+                else:
+                    document.authenticate(password)
+
+                    # remove the text input box after we're done with it
+                    if not document.is_encrypted:  # pylint: disable=no-member
+                        password_container.empty()
+
+                    try:
+                        df = parse_bank_statement(document, password)
+
+                    except ValueError as err:
+                        if err.args[0] == "document closed or encrypted":
+                            st.error("Wrong password. Please try again.")
+
+                    except (TypeError, WrongPasswordError):
+                        st.error("Wrong password. Please try again.")
+            else:
+                df = parse_bank_statement(document)
+
+        if df is not None:
+            dataframes.append(df)
+
+    if dataframes:
+        df = pd.concat(dataframes)
+        df = format_df(df)
+        csv = df.to_csv(index=False).encode("utf-8")
+        st.download_button(
+            label="Download CSV",
+            data=csv,
+            mime="text/csv",
         )
 
-        submit_button = st.button("Submit")
 
-        if submit_button:
-            if not password:
-                st.warning("Please enter a password.")
-            else:
-                document.authenticate(password)
-                try:
-                    parse_bank_statement(document, password)
-
-                except ValueError as err:
-                    if err.args[0] == "document closed or encrypted":
-                        st.error("Wrong password. Please try again.")
-
-                except (TypeError, WrongPasswordError):
-                    st.error("Wrong password. Please try again.")
-    else:
-        parse_bank_statement(document)
+if __name__ == "__main__":
+    app()
--- helpers.py
+++ helpers.py
@@ -1,18 +1,12 @@
 # pylint: disable=unsubscriptable-object
-from typing import Type
-
 import pandas as pd
 import streamlit as st
 from fitz import Document
 from monopoly.pipeline import Pipeline
-from monopoly.statements import BaseStatement
 from pydantic import SecretStr
 
 
-def parse_bank_statement(
-    document: Document, password: str = None
-) -> Type[BaseStatement]:
-    file_name = f"{document.pdf_file_name.split('.')[0]}.csv"
+def parse_bank_statement(document: Document, password: str = None) -> pd.DataFrame:
     pipeline = Pipeline(file_bytes=document.tobytes(), passwords=[SecretStr(password)])
     statement = pipeline.extract(safety_check=False)
     transactions = pipeline.transform(statement)
@@ -22,29 +16,22 @@ def parse_bank_statement(
             "This bank is not supported - transactions may be inaccurate", icon="⚠️"
         )
 
-    show_dataframe(transactions, file_name)
-    return statement
-
-
-def show_dataframe(transactions: list, file_name: str) -> None:
     df = pd.DataFrame(transactions)
+    return df
+
+
+def format_df(df: pd.DataFrame) -> pd.DataFrame:
     df["date"] = pd.to_datetime(df["date"]).dt.date
     df = df.drop(columns="suffix")
     total_balance = df["amount"].sum()
 
     # cosmetic changes
     df = df[["date", "description", "amount"]]
-    df.columns = ["Date", "Description", "Amount ($)"]
+    df.columns = ["Date", "Description", "Amount"]
     st.dataframe(
-        df.style.format({"Amount ($)": "{:.2f}"}),
+        df.style.format({"Amount": "{:.2f}"}),
         use_container_width=True,
         hide_index=True,
     )
     st.write(f"Total Balance: ${total_balance:.2f}")
-    csv = df.to_csv(index=False).encode("utf-8")
-    st.download_button(
-        label="Download CSV",
-        data=csv,
-        file_name=file_name,
-        mime="text/csv",
-    )
+    return df
