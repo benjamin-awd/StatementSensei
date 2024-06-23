369ab89362ed8792da5b62fc06894e7dea168bc0
Benjamin Dornel <benjamindornel@gmail.com>
2024-06-23 17:21:42 +0800

    refactor(streamlit): split up file and password handling

--- monopoly_streamlit/app.py
+++ monopoly_streamlit/app.py
@@ -2,21 +2,57 @@ import pandas as pd
 import streamlit as st
 from monopoly.pdf import WrongPasswordError
 from pymupdf import Document
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from monopoly_streamlit.helpers import format_df, parse_bank_statement
 from monopoly_streamlit.logo import logo
 
 
+def handle_file(file: UploadedFile):
+    file_bytes = file.getvalue()
+    document = Document(stream=file_bytes)
+
+    if document.is_encrypted:  # pylint: disable=no-member
+        return handle_encrypted_document(document, file.name)
+
+    return parse_bank_statement(document)
+
+
+def handle_encrypted_document(document: Document, file_name):
+    password_container = st.empty()
+    password = password_container.text_input(
+        label="Password",
+        type="password",
+        placeholder=f"Enter password for {file_name}",
+        key=file_name,
+    )
+
+    if not password:
+        st.warning("Please enter a password.")
+        return None
+
+    document.authenticate(password)
+
+    if not document.is_encrypted:  # pylint: disable=no-member
+        password_container.empty()
+        try:
+            return parse_bank_statement(document, password)
+        except (ValueError, WrongPasswordError):
+            st.error("Wrong password. Please try again.")
+            return None
+    else:
+        st.error("Failed to decrypt the document.")
+        return None
+
+
 def app() -> None:
     st.set_page_config(page_title="Monopoly", layout="wide")
-
     st.image(logo, width=350)
-
     st.markdown(
         """
         ## Convert bank statements to CSV
-    Effortlessly extract transactions from PDF bank statements.
-    """
+        Effortlessly extract transactions from PDF bank statements.
+        """
     )
 
     uploaded_files = st.file_uploader(
@@ -26,49 +62,10 @@ def app() -> None:
         accept_multiple_files=True,
     )
 
-    dataframes = []
-
-    for file in uploaded_files:
-        if file:
-            df = None
-            file_bytes = file.getvalue()
-            document = Document(stream=file_bytes)
-
-            if document.is_encrypted:  # pylint: disable=no-member
-                password_container = st.empty()
-                password = password_container.text_input(
-                    label="Password",
-                    type="password",
-                    placeholder=f"Enter password for {file.name}",
-                    key=file.name,
-                )
-
-                if not password:
-                    st.warning("Please enter a password.")
-                else:
-                    document.authenticate(password)
-
-                    # remove the text input box after we're done with it
-                    if not document.is_encrypted:  # pylint: disable=no-member
-                        password_container.empty()
-
-                    try:
-                        df = parse_bank_statement(document, password)
-
-                    except ValueError as err:
-                        if err.args[0] == "document closed or encrypted":
-                            st.error("Wrong password. Please try again.")
-
-                    except (TypeError, WrongPasswordError):
-                        st.error("Wrong password. Please try again.")
-            else:
-                df = parse_bank_statement(document)
-
-        if df is not None:
-            dataframes.append(df)
+    dataframes = [handle_file(file) for file in uploaded_files if file]
 
     if dataframes:
-        df = pd.concat(dataframes)
+        df = pd.concat([df for df in dataframes if df is not None])
         df = format_df(df)
         csv = df.to_csv(index=False).encode("utf-8")
         st.download_button(
