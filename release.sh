155b48b6abf13a27b09633ff28b5ddd923dea741
Benjamin Dornel <benjamindornel@gmail.com>
2024-06-24 23:42:16 +0800

    chore(tauri): pin monopoly-app cargo version to 0.1.0

--- release.sh
+++ release.sh
@@ -15,9 +15,6 @@ msg="# managed by release.sh"
 # update the pyproject version
 poetry version $new_version
 
-# update the Cargo.toml version
-sed -i "s/^version = \".*\"/version = \"$new_version\"/" tauri/src-tauri/Cargo.toml
-
 # update the tauri.conf.json version
 jq --arg new_version "$new_version" '.version = $new_version' tauri/src-tauri/tauri.conf.json > temp.json && mv temp.json tauri/src-tauri/tauri.conf.json
 
