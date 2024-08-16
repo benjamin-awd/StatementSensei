9133c0d24d25c64650943d9991268132c1ee60ee
Benjamin Dornel <benjamindornel@gmail.com>
2024-08-16 23:07:33 +0800

    refactor: move supported banks and app description to constants

---
+++ webapp/constants.py
@@ -0,0 +1,15 @@
+APP_DESCRIPTION = """
+## Convert bank statements to CSV
+Transform your PDF bank statements into easy-to-use CSV files.
+"""
+
+SUPPORTED_BANKS = """Currently supported banks:
+| Bank                | Credit Statement    | Debit Statement     |
+| --------------------| --------------------| --------------------|
+| Citibank            | ✅                 | ❌                  |
+| DBS/POSB            | ✅                 | ✅                  |
+| HSBC                | ✅                 | ❌                  |
+| Maybank             | ✅                 | ✅                  |
+| OCBC                | ✅                 | ✅                  |
+| Standard Chartered  | ✅                 | ❌                  |
+"""
