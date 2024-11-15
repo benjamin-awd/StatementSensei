5fe8bcc1f9982fc1b123fa7985b38c5f81642668
Benjamin Dornel <benjamindornel@gmail.com>
2024-11-15 23:34:26 +0800

    fix(build): add hash to statementsensei tar.gz

--- .github/hooks/include_webapp_in_requirements.sh
+++ .github/hooks/include_webapp_in_requirements.sh
@@ -3,14 +3,18 @@
 REQUIREMENTS_FILE="requirements.txt"
 VERSION=$(poetry version --short)
 TAR_FILE="dist/statement_sensei-$VERSION.tar.gz"
+HASH=$(sha256sum "$TAR_FILE" | awk '{ print $1 }')
 
 poetry export -f requirements.txt --output requirements.txt --extras ocrmypdf --without dev
 
 if [ -f "$REQUIREMENTS_FILE" ]; then
     FIRST_LINE=$(head -n 1 "$REQUIREMENTS_FILE")
-    if [ "$FIRST_LINE" != "$TAR_FILE" ]; then
-        echo "Adding $TAR_FILE to the top of $REQUIREMENTS_FILE"
-        (echo "$TAR_FILE" && cat "$REQUIREMENTS_FILE") > "$REQUIREMENTS_FILE.tmp"
+    # Create the required entry with hash
+    TAR_WITH_HASH="dist/statement_sensei-$VERSION.tar.gz --hash=sha256:$HASH"
+
+    if [ "$FIRST_LINE" != "$TAR_WITH_HASH" ]; then
+        echo "Adding $TAR_WITH_HASH to the top of $REQUIREMENTS_FILE"
+        (echo "$TAR_WITH_HASH" && cat "$REQUIREMENTS_FILE") > "$REQUIREMENTS_FILE.tmp"
         mv "$REQUIREMENTS_FILE.tmp" "$REQUIREMENTS_FILE"
     fi
 else
