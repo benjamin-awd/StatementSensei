1ea754aeb6d3622183bf0bd1f031d6391409ec7c
Benjamin Dornel <benjamindornel@gmail.com>
2025-07-12 13:34:11 +0800

    build: add support for non-macos types when modifying requirements.txt

--- .github/hooks/include_webapp_in_requirements.sh
+++ .github/hooks/include_webapp_in_requirements.sh
@@ -8,13 +8,17 @@ HASH=$(sha256sum "$TAR_FILE" | awk '{ print $1 }')
 # Export dependencies using uv (this adds -e . if project is local)
 uv export --output-file "$REQUIREMENTS_FILE" --all-extras
 
-# Remove "-e ." or any editable install lines
-sed -i '' '/^-e \.$/d' "$REQUIREMENTS_FILE"
+# Remove "-e ." or any editable install lines (macOS vs Linux compatible)
+# Detect OS for sed inline flag
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' '/^-e\s\+\.\s*$/d' "$REQUIREMENTS_FILE"
+else
+  sed -i '/^-e\s\+\.\s*$/d' "$REQUIREMENTS_FILE"
+fi
 
 # Add tar.gz with hash to the top of requirements.txt
 if [ -f "$REQUIREMENTS_FILE" ]; then
     FIRST_LINE=$(head -n 1 "$REQUIREMENTS_FILE")
-    # Create the required entry with hash
     TAR_WITH_HASH="dist/statement_sensei-$VERSION.tar.gz --hash=sha256:$HASH"
 
     if [ "$FIRST_LINE" != "$TAR_WITH_HASH" ]; then
